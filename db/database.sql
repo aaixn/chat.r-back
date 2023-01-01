ab2a8eb16fedcf7b6ecc7dc8d775a71f4a1c26d6
aaixn <axnguyen246@gmail.com>
2023-01-01 14:44:54 -0800

    create db

---
+++ db/database.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE users (
+--     id SERIAL NOT NULL PRIMARY KEY,
+--     username VARCHAR(50) NOT NULL UNIQUE,
+--     password VARCHAR(50) NOT NULL,
+--     pfp TEXT NOT NULL DEFAULT 'https://i.imgur.com/igF2kHr.png',
+--     friends INTEGER[],
+--     active BOOLEAN
+-- );
+
+-- CREATE TABLE messages (
+--     id SERIAL NOT NULL PRIMARY KEY,
+--     sender_id SERIAL NOT NULL REFERENCES users(id),
+--     receiver_id SERIAL NOT NULL REFERENCES users(id),
+--     content TEXT NOT NULL,
+--     time_stamp TIMESTAMP NOT NULL DEFAULT now(),
+--     read BOOLEAN
+-- );
